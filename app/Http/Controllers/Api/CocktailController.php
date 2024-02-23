af6f317f863351b505aa86c92ca5d0abc9499527
Youss0203 <youssefhalibi003@gmail.com>
2024-02-23 19:39:26 +0100

    iniziato ad aggiungere route per api

--- app/Http/Controllers/Api/CocktailController.php
+++ app/Http/Controllers/Api/CocktailController.php
@@ -4,8 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Cocktail;
 
 class CocktailController extends Controller
 {
-    //
+    public function index() {
+        $cocktails = Cocktail::all();
+        return response()->json([
+            "success" => true,
+            "results" => $cocktails
+        ]);
+    }
 }
