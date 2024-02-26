531fa32a9a5d7191b89c77e40631883801472557
Youss0203 <youssefhalibi003@gmail.com>
2024-02-26 09:47:39 +0100

    sistemata route api e mostrato array sul browser

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('/cocktails', ProjectController::class);
+Route::apiResource('/cocktails', CocktailController::class);
